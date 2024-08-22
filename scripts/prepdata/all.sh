db715b16614cd5be684cfe53ada73cb3884376f3
Stephen Piccolo <stephen.piccolo.byu@gmail.com>
2024-08-22 07:21:09 -0600

    Added notes

--- scripts/prepdata/all.sh
+++ scripts/prepdata/all.sh
@@ -2,17 +2,26 @@
 
 set -e
 
+# https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE20194
+# https://pubmed.ncbi.nlm.nih.gov/20064235/
 #bash /scripts/prepdata/gse20194.sh
+
+# https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE24080
+# https://pubmed.ncbi.nlm.nih.gov/20064235/
 bash /scripts/prepdata/gse24080.sh
+
+# https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE49711
+# https://pubmed.ncbi.nlm.nih.gov/25150839/
 #bash /scripts/prepdata/gse49711.sh
 
 
 #bash /scripts/prepdata/bladderbatch.sh
 #Other possibilities:
 #https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE47792 (SEQC superseries)
+#https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE49711 (same as GSE49711 but uses Agilent microarrays)
 #https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE54275 (specifically, the samples for GPL15932)
-#https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE25507
-#https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE65204
-#https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE58979
-#https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE19750
+#https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE25507 (Affymetrix Human Genome U133 Plus 2.0)
+#https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE65204 (Agilent)
+#https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE58979 (Affymetrix PrimeView)
+#https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE19750 (Affymetrix Human Genome U133 Plus 2.0)
 #bash /scripts/prepdata/tcga.sh
