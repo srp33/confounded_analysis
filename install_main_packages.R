a042caf562c46255e2e6dc6b9caf4f5e89a11266
Preston Raab <prestonraab@gmail.com>
2025-05-21 12:00:33 -0600

    Install packages

--- install_main_packages.R
+++ install_main_packages.R
@@ -1,6 +1,7 @@
-packages = c("pacman", "dplyr", "readr", "stringr", "tidyr", "tibble", "ggplot2", "gridExtra", "png", "magick", "colorspace", "pracma", "kableExtra", "Rtsne", "argparse", "docstring", "R.devices", "doParallel", "readxl", "bladderbatch", "sva", "SCAN.UPC")
-
 if (!requireNamespace("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
+
+#packages = c("tidyverse", "doParallel", "readxl", "sva", "SCAN.UPC")
+packages = c("pacman", "dplyr", "readr", "stringr", "tidyr", "tibble", "ggplot2", "gridExtra", "png", "magick", "colorspace", "pracma", "kableExtra", "Rtsne", "argparse", "docstring", "R.devices", "doParallel", "readxl", "sva", "SCAN.UPC")
 
 BiocManager::install(packages)
