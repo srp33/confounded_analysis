eaa7eefaebe1adda5ba31c27918d54753ae4bf1a
Preston Raab <prestonraab@gmail.com>
2025-05-28 10:28:45 -0600

    Create archive for metrics

--- run_docker.sh
+++ run_docker.sh
@@ -7,7 +7,7 @@ image=srp33/confounded-paper:version1
 docker build -t $image .
 
 mkdir -p data/gse20194 data/gse24080 data/gse49711
-mkdir -p outputs/figures outputs/metrics outputs/optimizations outputs/tables
+mkdir -p outputs/figures outputs/metrics outputs/optimizations outputs/tables outputs/metrics/archive
 
 #docker run -d --rm \
 docker run -i -t --rm \
