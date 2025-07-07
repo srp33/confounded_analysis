5e0836165c5ab7f1a0420659f2106521c226fad0
Preston Raab <prestonraab@gmail.com>
2025-07-07 12:05:05 -0600

    Add new R libraries

--- install_adjuster_specific_packages.R
+++ install_adjuster_specific_packages.R
@@ -7,6 +7,12 @@ BiocManager::install(tampor_packages)
 
 BiocManager::install("BatchQC")
 
+BiocManager::install(c("batchelor", "rliger"))
+
 devtools::install_github("satijalab/seurat", "seurat5")
 devtools::install_github("satijalab/seurat-data", "seurat5")
 
+
+# Needed for FairAdapt
+install.packages("ranger")
+install.packages("fairadapt")
