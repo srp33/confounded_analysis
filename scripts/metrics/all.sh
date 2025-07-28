b350f2c0fb84fab6682219766ff11678439f2324
Preston Raab <prestonraab@gmail.com>
2025-07-28 14:48:15 -0600

    Add reduction to metrics

--- scripts/metrics/all.sh
+++ scripts/metrics/all.sh
@@ -2,9 +2,12 @@
 
 set -e
 
+# python /scripts/metrics/hist_gradient_er_classification.py
+
 # bash /scripts/metrics/mutual_info.sh
+bash /scripts/metrics/reduce.sh
+bash /scripts/metrics/classify.sh
 # bash /scripts/metrics/mse.sh
 # bash /scripts/metrics/mmd.sh
-bash /scripts/metrics/classify.sh
 
 # python /scripts/metrics/eval_refine.py
