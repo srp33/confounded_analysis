e19461bb688f20ba0194695e68c433d2c08d88db
Preston Raab <prestonraab@gmail.com>
2025-09-04 22:08:23 -0600

    Update

--- build_apptainer.sh
+++ build_apptainer.sh
@@ -1,2 +1,3 @@
+#!/bin/sh
 module load apptainer
 apptainer build remove-batch-effects.sif apptainer.def
--- scripts/all.sh
+++ scripts/all.sh
@@ -8,6 +8,7 @@ id
 # Set PYTHONPATH for all Python scripts
 export PYTHONPATH="/scripts:$PYTHONPATH"
 
-# bash /scripts/prepdata/all.sh &> /outputs/prepdata.log
+bash /scripts/prepdata/all.sh &> /outputs/prepdata.log
 # bash /scripts/adjust/all.sh &> /outputs/adjust.log
 bash /scripts/evaluations/all.sh
+
