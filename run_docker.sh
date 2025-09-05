0c8501aa7edd6bf26e012d76baaf6b5695c2ffae
Preston Raab <prestonraab@gmail.com>
2025-09-04 21:49:21 -0600

    Revert docker file for kumiko usage

--- run_docker.sh
+++ run_docker.sh
@@ -6,25 +6,27 @@ image=remove-batch-effects:latest
 
 DOCKER_BUILDKIT=1 docker build -t $image .
 
-SHARED_DIR=$(pwd)/../groups/
+SHARED_DIR=$(pwd)
+SCRIPTS_DIR=$(pwd)
+
 
 mkdir -p $SHARED_DIR/data $SHARED_DIR/data/.cache 
 mkdir -p $SHARED_DIR/outputs/figures/ $SHARED_DIR/outputs/metrics $SHARED_DIR/outputs/tables
 mkdir -p $SHARED_DIR/tmp
 
-GROUP_ID=$(getent group grp_batch_effects | cut -d: -f3)
+GROUP_ID=$(getent group docker | cut -d: -f3)
 
 #docker run -d --rm \
 docker run -i -t --rm \
   --user $(id -u):$GROUP_ID \
   --env HOME=/ \
-  -v $(pwd)$SHARED_DIR/data:/data \
-  -v $(pwd)$SHARED_DIR/outputs:/outputs \
-  -v $(pwd)/scripts:/scripts \
-  -v $(pwd)$SHARED_DIR/tmp:/tmp \
-  -v $(pwd)/data/.cache:/.cache \
-  -v $(pwd)/tmp/matplotlib/config:/.config/matplotlib \
-  -v $(pwd)/tmp/matplotlib/cache:/.cache/matplotlib \
+  -v $SHARED_DIR/data:/data \
+  -v $SHARED_DIR/outputs:/outputs \
+  -v $SCRIPTS_DIR/scripts:/scripts \
+  -v $SHARED_DIR/tmp:/tmp \
+  -v $SCRIPTS_DIR/data/.cache:/.cache \
+  -v $SCRIPTS_DIR/tmp/matplotlib/config:/.config/matplotlib \
+  -v $SCRIPTS_DIR/tmp/matplotlib/cache:/.cache/matplotlib \
   $image \
   bash -c /scripts/all.sh
 
