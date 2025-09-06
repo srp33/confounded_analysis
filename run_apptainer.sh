b1d913024e3876bdf9a7c975d14c7a5e91c65c30
Preston Raab <prestonraab@gmail.com>
2025-09-05 22:43:59 -0600

    Update run_apptainer for group access

--- run_apptainer.sh
+++ run_apptainer.sh
@@ -6,6 +6,9 @@ image="remove-batch-effects.sif"
 
 SHARED_DIR=$(pwd)/../groups/grp_batch_effects
 
+echo "Shared dir:"
+echo $SHARED_DIR
+
 mkdir -p $SHARED_DIR/data $SHARED_DIR/data/.cache 
 mkdir -p $SHARED_DIR/outputs/figures/ $SHARED_DIR/outputs/metrics $SHARED_DIR/outputs/tables
 mkdir -p $SHARED_DIR/tmp
@@ -13,13 +16,9 @@ mkdir -p $SHARED_DIR/tmp
 module load apptainer
 
 apptainer exec \
-  --bind "$(pwd)$SHARED_DIR/data":/data \
-  --bind "$(pwd)$SHARED_DIR/outputs":/outputs \
+  --bind "$SHARED_DIR/data":/data \
+  --bind "$SHARED_DIR/outputs":/outputs \
   --bind "$(pwd)/scripts":/scripts \
-  --bind "$(pwd)$SHARED_DIR/tmp":/tmp \
-  --bind "$(pwd)/data/.cache":/.cache \
-  --bind "$(pwd)/tmp/matplotlib/config":/.config/matplotlib \
-  --bind "$(pwd)/tmp/matplotlib/cache":/.cache/matplotlib \
-  --env HOME=/ \
-  $image \
+  --bind "$SHARED_DIR/data/.cache":/.cache \
+  $SHARED_DIR/$image \
   /scripts/all.sh
