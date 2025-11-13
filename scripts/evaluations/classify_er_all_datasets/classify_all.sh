1b5f09d6f507f085cf60d31a8950ff64ece1f267
Amanda Warren <aw998@byu.edu>
2025-11-13 15:26:39 -0700

    classify unadjusted files

--- scripts/evaluations/classify_er_all_datasets/classify_all.sh
+++ scripts/evaluations/classify_er_all_datasets/classify_all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=classify_all
-#SBATCH --array=0-3 # One job per adjuster folder
+#SBATCH --array=0 # One job per adjuster folder
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=16G
@@ -10,9 +10,18 @@
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
 ANALYSIS_DIR=~/confounded_analysis
 CLASSIFIER_SCRIPT=/scripts/evaluations/classify_er_all_datasets/run_classifier.sh
+UNADJUSTED_SCRIPT=/scripts/evaluations/classify_er_all_datasets/classify_unadjusted.sh
 
 echo "Running classification with $CLASSIFIER_SCRIPT"
+echo "Running classification on unadjusted files with $UNADJUSTED_SCRIPT"
 echo "Job ID: $SLURM_JOB_ID"
 
 cd $ANALYSIS_DIR
-bash $ANALYSIS_DIR/run_in_apptainer.sh $CLASSIFIER_SCRIPT "$SLURM_ARRAY_TASK_ID"
+#bash $ANALYSIS_DIR/run_in_apptainer.sh $CLASSIFIER_SCRIPT "$SLURM_ARRAY_TASK_ID"
+
+if [ "$SLURM_ARRAY_TASK_ID" -eq 0 ]; then
+    echo "Running classification on unadjusted files with $UNADJUSTED_SCRIPT"
+    bash $ANALYSIS_DIR/run_in_apptainer.sh $UNADJUSTED_SCRIPT
+else 
+    echo "Skipping unadjusted classification for task $SLURM_ARRAY_TASK_ID"
+fi
